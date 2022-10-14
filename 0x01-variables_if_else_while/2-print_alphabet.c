146c7f299280017f38e5e86545f6225d7c1f7efe
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-14 16:14:59 -0700

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - convert to lowercase
+ * Return: always zero normally
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc < 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	return (0);
+}
+
+
