c9a4e16b971ba1f5682d346ff2d829c163ca6519
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-13 17:39:31 -0700

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/** main - prints with proper grammar, but 
+/** 
+  * main - prints with proper grammar, but 
   * the outcome is a piece of art
   * Return: 0 if exited properly, non-zero otherwise
   */
