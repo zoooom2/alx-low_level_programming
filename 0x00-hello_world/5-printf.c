7011aff4d248337bbe628a742078a5e7ad00825e
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-13 17:48:05 -0700

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-
-/** 
- * main - prints with proper grammar, 
- * but the outcome is a piece of art
- * Return: 0 if exited properly, non-zero otherwise
- */
+/** main - prints with proper grammar */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
