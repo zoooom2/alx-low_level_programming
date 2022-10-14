c225eac693cdcc2e72a130d6bbdb5b00ac54b1c1
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-14 16:25:51 -0700

    new commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets
+ * Return: always zero
+ */
+int main(void)
+{
+	char alpha;
+	char lower;
+
+	for (alpha = 'a', alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for(lower = 'A', lower <='Z'; lower++)
+		putchar(lower);
+	putchar('\n');
+	return (0);
+}
+
