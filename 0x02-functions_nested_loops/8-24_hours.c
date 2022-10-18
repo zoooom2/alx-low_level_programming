22ff5841710b6316d7ce459cf8d0e638a9d59b43
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-18 23:54:09 +0100

    new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 
    for(hrs=0; hrs< 24; hrs++)
    {
-    for(min=0; min<24; min++)
+    for(min=0; min<60; min++)
     {
         _putchar((hrs / 10) + '0');
         _putchar((hrs % 10) + '0');
