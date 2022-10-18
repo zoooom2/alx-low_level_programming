136eb0287ad6cb476fb015f9199a4b9a1917d7a0
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-19 00:19:40 +0100

    new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,10 @@
 #include "main.h"
 
 /**
- * main - it should print a valid string
- * Return: always return zero if exited properly
+ * main - print Holberton, followed by
+ * a new line
+ * Return: always return zero if exited
+ * properly
  */
 int main(void)
 {
