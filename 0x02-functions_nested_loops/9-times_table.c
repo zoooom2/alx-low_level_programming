4fb8a241bb9ffc374d78f8b1295293efc4000edd
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-18 23:58:17 +0100

    new commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
     {
         for (j=0; j<10; j++)
         {
-            int result = i * j;
+            int result = (i + '0') * (j + '0');
             _putchar(result);
             _putchar(',');
             _putchar(' ');
