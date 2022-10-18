3add1e1156f4da7fc03aa6b974148a144a2275be
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-19 00:13:32 +0100

    new commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
    if(n > 98)
    {
-    while(n >= 98)
+    while(n > 98)
     {
         printf("%d, ", n);
         n--;
@@ -18,15 +18,12 @@ void print_to_98(int n)
    }
    else if (n < 98)
    {
-    while(n <= 98)
+    while(n < 98)
     {
         printf("%d, ", n);
         n++;
     }
    }
-   else{
-    printf("%d", n);
-   }
-   _putchar('\n');
+    printf("%d\n", 98);
     return;
 }
