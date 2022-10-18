ad25013278a3dc47a12b736c0aa29528417a0de1
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-18 13:14:09 -0700

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - it should print a valid string
+ * Return: always return zero if exited properly
+ */
+main(void)
+{
+	putchar("_");
+	return 0
+}
