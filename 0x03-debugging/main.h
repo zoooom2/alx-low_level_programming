0df65130a4c859893b39d8d8a1820b8c879b6fdc
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 20:20:03 +0100

    new commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,6 +7,6 @@ void positive_or_negative(int i);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 int main(void);
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
