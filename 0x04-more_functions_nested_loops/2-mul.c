65a7403936989c14ad544d848d868a592e1e30f4
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 13:16:04 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - to check if its digit
+ * Return: Always 1 if it's digit
+ * @a: is the first parameter
+ * @b: is the second parameter
+ *
+ */
+int mul(int a, int b)
+{
+    return a * b;
+}
