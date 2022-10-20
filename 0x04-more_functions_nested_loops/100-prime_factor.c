eb731c7da7151c6651a0c29ed502b2608f0b44e0
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 09:29:04 -0700

    new commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,19 +8,17 @@
  */
 int main(void)
 {
-    long x, maxf;
-    long number = 612852475143;
-    double square = sqrt(number);
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
 
-    for (x = 1; x <= square; x++)
-    {
-        if (number % x == 0)
-        {
-            maxf = number / x;
-        }
-    }
-
-    printf("%ld\n", maxf);
-
-    return (0);
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
 }
