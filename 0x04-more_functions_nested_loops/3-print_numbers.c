76bf7585c8b86f22cceb129e182d4e7f7efc6659
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 13:34:58 +0100

    new commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,13 @@
 /**
  * main - to print number from 0 t0 9
  * followed by a new line
- * Return: Always 1 if it's digit
+ * Return: void
  *
  */
 void print_numbers(void)
 {
     int i;
+
     for (i = 0; i < 10; i++)
         _putchar(i + '0');
     _putchar('\n');
