1d8af556199e0838858826b6773cf283fb2ee57f
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 12:56:00 -0700

    new commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,13 @@
 #include "main.h"
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
-    int i;
+	int i;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    i = 0;
-=======
-    i = -98;
->>>>>>> a97ad0e7e87797d0f209d27432f830a678dbf76e
-=======
-    i = 0;
->>>>>>> 36ae463905f55462f0fa48d0e84bbf5b621c3c01
-    positive_or_negative(i);
-
-    return (0);
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
