295ae6b4402371066e59316ae0916c91c35b4cdc
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 13:19:19 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - to check if its digit
+ * Return: Always 1 if it's digit
+ *
+ */
+void print_numbers(void)
+{
+    int i;
+    for (i = 0; i < 10; i++)
+        _putchar(i + '0');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _isupper(int c);
 int _isdigit(int d);
 int mul(int a, int b);
+void print_numbers(void);
