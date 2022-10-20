b4666d498484920732f6373d520fb8b17733dc96
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 16:01:05 +0100

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,21 +7,21 @@
 
 void fizz_buzz(void)
 {
-    int i = 0;
+    int i;
 
     for (i = 0; i < 100; i++)
     {
         if (i % 3 == 0)
         {
-            printf("Fizz");
+            printf("Fizz ");
         }
         else if (i % 5)
         {
-            printf("Buzz");
+            printf("Buzz ");
         }
         else if (i % 3 && i % 5)
         {
-            printf("FizzBuzz");
+            printf("FizzBuzz ");
         }
         else
         {
