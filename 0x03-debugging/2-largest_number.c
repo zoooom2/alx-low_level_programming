7f667503823bc1e159e03ab40f65e6c4127ea1f1
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-20 20:28:28 +0100

    new coommit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,11 +11,11 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && a > c)
+    if ((a > b && a > c) || ((a == b || a == c) && (a > b || a > c)))
     {
         largest = a;
     }
-    else if (b > a && b > c)
+    else if ((b > a && b > c) || ((b == a || b == c) && (b > a || b > c)))
     {
         largest = b;
     }
@@ -23,6 +23,5 @@ int largest_number(int a, int b, int c)
     {
         largest = c;
     }
-
     return (largest);
 }
