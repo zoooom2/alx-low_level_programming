ecd155e058d731a920e3ba40d8c043b682545b3e
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-27 13:24:41 +0100

    new commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -20,12 +20,8 @@ int _strcmp(char *s1, char *s2)
     {
         return (0);
     }
-    else if (s1[index] != s2[index] && s1[index] > s2[index])
+    else if (s1[index] != s2[index])
     {
-        return (1);
-    }
-    else if (s1[index] != s2[index] && s1[index] < s2[index])
-    {
-        return (-1);
+        return (s1[index] - s2[index]);
     }
 }
