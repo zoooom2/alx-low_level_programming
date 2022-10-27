0dc53c7f2a937b00b16422455f04ed54d4704174
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-28 00:31:59 +0100

    new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,36 @@
-void print_number(int n);
+#include "main.h"
+/**
+ * @brief a function that prints an integer
+ *
+ * @param n
+ */
+
+void print_number(int n)
+{
+    int i;
+    int tmp = 0;
+    int divider = 10;
+    int power = 1;
+    int num;
+
+    while ((n / divider) > 1)
+    {
+        divider *= 10;
+        tmp++;
+    }
+
+    for (i = tmp; i > 0; i--)
+    {
+        tmp = i;
+        while (tmp != 0)
+        {
+            power *= 10;
+            tmp--;
+        }
+        num = n / power;
+
+        _putchar(num + '0');
+
+        n = n % power;
+    }
+}
