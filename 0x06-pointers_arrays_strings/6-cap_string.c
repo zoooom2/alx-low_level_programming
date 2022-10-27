07de86db21c9d2287b2b6ed2452fecae9d5e59d0
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-27 14:33:07 +0100

    new commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *word)
 
     while (word[index] != '\0')
     {
-        if (index == 0 || word[index - 1] == 32 || word[index - 1] == 9 || word[index - 1] == '\n' || word[index - 1] == ',' || word[index - 1] == ';' || word[index - 1] == '.' || word[index - 1] == '!' || word[index - 1] == '?' || word[index - 1] == '"' || word[index - 1] == '(' || word[index - 1] == ')' || word[index - 1] == '{' || word[index - 1] == '}')
+        if (word[index - 1] == ' ' || word[index - 1] == '\t' || word[index - 1] == '\n' || word[index - 1] == ',' || word[index - 1] == ';' || word[index - 1] == '.' || word[index - 1] == '!' || word[index - 1] == '?' || word[index - 1] == '"' || word[index - 1] == '(' || word[index - 1] == ')' || word[index - 1] == '{' || word[index - 1] == '}')
         {
             if (word[index] >= 97 || word[index] <= 122)
             {
