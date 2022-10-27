07cc1c8ff0a063603323a30c8fc79ad2e01e6e0a
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-27 17:27:58 +0100

    nc

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 char *leet(char *words)
 {
     char table[10] = {'a', 'A', 'o', 'O', 'e', 'E', 't', 'T', 'l', 'L'};
-    int num[10] = {4, 4, 0, 0, 3, 3, 7, 7, 1, 1};
+    char num[10] = {'4', '4', '0', '0', '3', '3', '7', '7', '1', '1'};
     int index, idx;
 
     for (index = 0; index != '\0'; index++)
