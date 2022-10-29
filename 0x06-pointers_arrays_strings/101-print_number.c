b6a3593b97d7fb5f5b01c02dd3c30d313cabfdce
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-29 05:02:00 +0100

    new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,26 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * rot13 - encoder rot13
- * @s: pointer to string params
- *
- * Return: *s
- */
-char *rot13(char *s)
-{
-    int i;
-    int j;
-    char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    for (i = 0; s[i] != '\0'; i++)
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ **/
+
+void print_number(int n)
+{
+    unsigned int n1;
+
+    n1 = n;
+
+    if (n < 0)
     {
-        for (j = 0; j < 52; j++)
-        {
-            if (s[i] == data1[j])
-            {
-                s[i] = datarot[j];
-                break;
-            }
-        }
+        _putchar('-');
+        n1 = -n;
     }
-    return (s);
+
+    if (n1 / 10 != 0)
+    {
+        print_number(n1 / 10);
+    }
+    _putchar((n1 % 10) + '0');
 }
