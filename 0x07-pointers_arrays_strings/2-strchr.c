148f837ec48c9a09312fb6e54ae7d87fd48bb53f
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-31 22:36:24 +0100

    NEW

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a string
+ *
+ * @s: string
+ * @c: character
+ * @return char*
+ */
+char *_strchr(char *s, char c)
+{
+    int i, j;
+    char *p;
+
+    for (i = 0; i != '\0'; i++)
+    {
+        if (s[i] == c)
+        {
+            for (j = 0; j != '\0'; j++)
+            {
+                p[j] = s[i];
+            }
+            p[j] = '\0';
+            return (p);
+        }
+        else if (s[i + 1] == '\0')
+        {
+            return ("");
+        }
+    }
+}
