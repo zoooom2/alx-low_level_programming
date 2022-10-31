7114f12e10dbdbe4233c008fdf8071efb19eb815
Roqeeb <roqeebadeyinka@gmail.com>
2022-11-01 00:30:42 +0100

    new

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
     {
         for (j = 0; s[j] != '\0'; j++)
         {
-            if (s[j + 1] == '\0' && accept[i + 1] == '\0')
+            if ((s[j + 1] == '\0') && (accept[i + 1] == '\0'))
             {
                 count += 1;
             }
