96a327ba2ac631530f4110ebe565ff4bd841e412
Roqeeb <roqeebadeyinka@gmail.com>
2022-11-01 00:00:19 +0100

    new commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: The string
+ * @accept: The string to check
+ * @return unsigned
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    int i, j;
+    unsigned count = 0;
+
+    for (i = 0; i != '\0'; i++)
+    {
+        for (j = 0; j != '\0'; j++)
+        {
+            if (s[j] == accept[i])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
