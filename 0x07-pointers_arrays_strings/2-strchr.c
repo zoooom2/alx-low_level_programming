07ba8b92a6ae8d01e5421c03fcb11b8640c31cca
Roqeeb <roqeebadeyinka@gmail.com>
2022-10-31 23:02:00 +0100

    new

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,18 @@
 char *_strchr(char *s, char c)
 {
     int i, j;
+    char *p;
 
     for (i = 0; i != '\0'; i++)
     {
         if (s[i] == c)
         {
-            return (s[i]);
+            for (j = 0; j != '\0'; j++)
+            {
+                p[j] = s[i];
+                i++;
+            }
+            return p;
             break;
         }
         else if (s[i + 1] == '\0')
