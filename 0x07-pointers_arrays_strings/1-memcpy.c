8442bdad06566924a4b3b1b5bfc8e803c7c2c5f0
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-31 17:28:40 -0700

    new commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @dest: destination
  * @src: source
  * @n: number
- * @return char*
+ * Return: char*
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -17,5 +17,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	{
 		dest[i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
