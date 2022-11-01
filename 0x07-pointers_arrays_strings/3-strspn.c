0e7811d5789e0f27a54fac19e6147ae7a20ec28a
adeyinka roqeeb <roqeebadeyinka@gmail.com>
2022-10-31 17:16:30 -0700

    new commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,16 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j;
-
-    i = 0;
-    while (s[i] != '\0')
-    {
-        j = 0;
-        while (accept[j] != '\0' && s[i] != accept[j])
-            j++;
-        if (accept[j] == '\0')
-            return (i);
-        i++;
-    }
-    return (i);
+	unsigned int i, j;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0' && s[i] != accept[j])
+			j++;
+		if (accept[j] == '\0')
+			return (i);
+		i++;
+	}
+	return (i);
 }
